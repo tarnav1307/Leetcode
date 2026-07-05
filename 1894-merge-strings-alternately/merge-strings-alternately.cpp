a8e6fbede812b5d174a01b9ea4404b55e46a9ae8
tarnav1307 <arnavtripathi1306@gmail.com>
2026-07-05 19:35:42 +0530

    Time: 4 ms (31.70%) | Memory: 8.9 MB (7.73%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,19 +3,18 @@ public:
     string mergeAlternately(string word1, string word2) {
         int i = 0, j = 0;
         string ans;
-        
+        ans.reserve(word1.size() + word2.size()); // Avoid reallocations
+
         while (i < word1.size() && j < word2.size()) {
             ans.push_back(word1[i++]);
             ans.push_back(word2[j++]);
         }
 
-        while (i < word1.size()) {
+        while (i < word1.size())
             ans.push_back(word1[i++]);
-        }
 
-        while (j < word2.size()) {
+        while (j < word2.size())
             ans.push_back(word2[j++]);
-        }
 
         return ans;
     }
